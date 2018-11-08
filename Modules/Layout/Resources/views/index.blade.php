4e473d46735a8bc82c1e783097be1579f3073ff1
marfrelen <marfrelen@gmail.com>
2018-11-08 10:18:44 +0100

    + init springmal

---
+++ Modules/Layout/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layout::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('layout.name') !!}
+    </p>
+@stop
